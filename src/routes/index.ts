5451397514c5a00c2ada9163b3e5b0a4a81d9422
Gabriela Carvalho Camilo <gabccamilo.dev@gmail.com>
2020-09-14 22:59:36 -0300

    :construction: Adding basic server

---
+++ src/routes/index.ts
@@ -0,0 +1,17 @@
+import { Router } from 'express';
+
+const routes = Router();
+
+routes.get('/', (req, res) => res.json({
+  title: 'API Boilerplate 🚧',
+  technologies: ['TypeScript', 'Node', 'Express'],
+  version: '1.0.0',
+  author: {
+    name: 'Gabriela Carvalho Camilo',
+    gitHub: 'http://github.com/gabcamilo',
+    linkedIn: 'https://www.linkedin.com/in/gabccamilo/',
+  },
+  licence: 'MIT',
+}));
+
+export default routes;
---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import routes from './routes';
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+const port = process.env.PORT || 3333;
+app.listen(port, () => {
+  console.log(`[SERVER] 🚀 Server running on port ${port}`)
+});
