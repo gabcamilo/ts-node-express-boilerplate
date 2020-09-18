d60b73b706fdbc82e2c3f66d74e8693f96acdd89
Gabriela Carvalho Camilo <gabccamilo.dev@gmail.com>
2020-09-18 20:32:09 -0300

    :goal_net: Adding global error handler

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,7 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
+
+import AppError from './errors/AppError';
 
 import './database';
 
@@ -8,6 +11,20 @@ const app = express();
 
 app.use(express.json());
 app.use(routes);
+
+app.use((err: Error, req: Request, res: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+  console.error(err);
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
 
 const port = process.env.PORT || 3333;
 app.listen(port, () => {
