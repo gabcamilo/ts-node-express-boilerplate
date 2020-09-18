97511e2c665ea17e979d289b1f3e87d2006901eb
Gabriela Carvalho Camilo <gabccamilo.dev@gmail.com>
2020-09-18 00:13:12 -0300

    :construction: Adding create user

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,17 +1,23 @@
 import { Router } from 'express';
 
+import usersRouter from './users.routes';
+
 const routes = Router();
 
-routes.get('/', (req, res) => res.json({
-  title: 'API Boilerplate 🚧',
-  technologies: ['TypeScript', 'Node', 'Express'],
-  version: '1.0.0',
-  author: {
-    name: 'Gabriela Carvalho Camilo',
-    gitHub: 'http://github.com/gabcamilo',
-    linkedIn: 'https://www.linkedin.com/in/gabccamilo/',
-  },
-  licence: 'MIT',
-}));
+routes.use('/users', usersRouter);
+
+routes.get('/', (req, res) =>
+  res.json({
+    title: 'API Boilerplate 🚧',
+    technologies: ['TypeScript', 'Node', 'Express', 'TypeOrm', 'PostgreSQL'],
+    version: '1.0.0',
+    author: {
+      name: 'Gabriela Carvalho Camilo',
+      gitHub: 'http://github.com/gabcamilo',
+      linkedIn: 'https://www.linkedin.com/in/gabccamilo/',
+    },
+    licence: 'MIT',
+  }),
+);
 
 export default routes;
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
 import express from 'express';
-import routes from './routes';
 
 import './database';
+
+import routes from './routes';
 
 const app = express();
 
